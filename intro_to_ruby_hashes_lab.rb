ab7a8d8a804ce04f05b0db7fe1e133cb7d3af411
Dakota Willingham <kota@vaga.tk>
2020-02-19 20:24:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {railroads: 
+  {railroads:
+    :names = 
     {rent_in_dollars: 
       {
         one_piece_owned: 25,
