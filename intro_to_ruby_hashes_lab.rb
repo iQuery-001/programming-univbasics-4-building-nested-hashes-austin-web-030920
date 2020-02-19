6dc3db099ec03bd62bb340f7b49d08520e3b4524
Dakota Willingham <kota@vaga.tk>
2020-02-19 20:25:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,9 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {railroads:
-    :names = 
+    :names = {
+      reading_railroad: 
+    }
     {rent_in_dollars: 
       {
         one_piece_owned: 25,
